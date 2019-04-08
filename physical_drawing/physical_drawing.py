2b8513e935d2c8211bb181c07b27326bb8a2f9c9
paressuex11 <1138514846@qq.com>
2019-04-08 21:05:40 +0800

    0

--- physical_drawing/physical_drawing.py
+++ physical_drawing/physical_drawing.py
@@ -1,1 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+X=np.arange(-np.pi,np.pi,0.1)
+
+Ysin=np.sin(X)
+
+Ycos=np.cos(X)
+plt.plot(X,Ysin,"b-",label='Ysin',linewidth=2.0)
+plt.plot(X,Ysin*2,color="red",linestyle="--",linewidth=2.0,label="Ysin *2")
+plt.plot(X,Ycos,"c-.",linewidth=2.0,label="Ycos")
+plt.plot(X,Ycos*2,color="black",linestyle=":",linewidth=2.0,label="Ycos*2")
+
+plt.legend()
+plt.grid(True)
+plt.show()
